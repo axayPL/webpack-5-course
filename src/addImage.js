0abbbf2512a764b699cfa9a72c48bbcc55329e6d
MAKOLAB-NET\adam.boberek <adam.boberek@makolab.com>
2023-12-01 16:53:54 +0100

    rules for assets

--- src/addImage.js
+++ src/addImage.js
@@ -1,8 +1,9 @@
 import image from './image.png'
+import imageAlt from './assetSourceExample.txt'
 
 export default function addImage() {
     const img = document.createElement('img');
-    img.alt = 'water';
+    img.alt = imageAlt;
     img.height = 300;
     img.src = image;
     document.body.appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,24 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'dist/'
     },
     mode: 'none',
     module: {
         rules: [
             {
                 test: /\.(png|jpg)$/,
-                type: 'asset/resource'
+                type: 'asset', // Can be 'asset/resource' to handle it in separate file.
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 3 * 1024 // 3 kb - if below it will be asset/inline otherwise asset/resource.
+                    }
+                }
+            },
+            {
+                test: /\.txt/,
+                type: 'asset/source' // Be placed directly in your code.
             }
         ]
     }
