8e166487a5242e31cbd7f5e7ae2a496f96c86a18
axayPL <adamboberek@gmail.com>
2023-12-02 13:49:49 +0100

    add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,62 @@
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: 'dist/'
-    },
-    mode: 'none',
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpg)$/,
-                type: 'asset', // Can be 'asset/resource' to handle it in separate file.
-                parser: {
-                    dataUrlCondition: {
-                        maxSize: 3 * 1024 // 3 kb - if below it will be asset/inline otherwise asset/resource.
-                    }
-                }
-            },
-            {
-                test: /\.txt/,
-                type: 'asset/source' // Be placed directly in your code.
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [ '@babel/env' ],
-                        plugins: [ '@babel/plugin-transform-class-properties' ]
-                    }
-                }
-                
-            }
-        ]
-    },
-    plugins: [
-        new TerserPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'styles.[contenthash].css'
-        })
-    ]
-}
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "dist/",
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset", // Can be 'asset/resource' to handle it in separate file.
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024, // 3 kb - if below it will be asset/inline otherwise asset/resource.
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source", // Be placed directly in your code.
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-transform-class-properties"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "styles.[contenthash].css",
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        path.join(proces.cwd(), 'build/**/*')
+      ],
+    }),
+  ],
+};
